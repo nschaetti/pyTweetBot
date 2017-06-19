9e6347ce6ac386ed07165bdd9bd089fe37b76961
nschaetti <n.schaetti@gmail.com>
2017-06-19 19:39:08 +0200

    News and RSS

--- tweet/GoogleNewsHunter.py
+++ tweet/GoogleNewsHunter.py
@@ -4,6 +4,7 @@
 
 # Import
 from .Hunter import Hunter
+from news.GoogleNewsClient import GoogleNewsClient
 
 
 # Hunter for Google News
@@ -13,7 +14,7 @@ class GoogleNewsHunter(Hunter):
     """
 
     # Constructor
-    def __init__(self, search_term, lang, lang_type, n_pages=2):
+    def __init__(self, search_term, lang, country, n_pages=2):
         """
         Constructor
         :param search_term: Search term
@@ -22,8 +23,11 @@ class GoogleNewsHunter(Hunter):
         """
         self._search_term = search_term
         self._lang = lang
-        self._lang_type = lang_type
+        self._country = country
         self._n_pages = n_pages
+        self._google_news_client = GoogleNewsClient(search_term, lang, country)
+        self._news = list()
+        self._current_page = 0
     # end __init__
 
     # Iterator
