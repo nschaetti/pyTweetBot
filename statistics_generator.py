614ccb0ec1e1b1e30903c94f97c612dd97f122b5
Nils Schaetti <n.schaetti@gmail.com>
2017-07-10 21:04:18 +0200

    Statistical model

--- statistics_generator.py
+++ statistics_generator.py
@@ -62,9 +62,8 @@ if __name__ == "__main__":
         # For each tweet
         for tweet in page:
             if not tweet.retweeted:
-                print(tweet.__dict__.keys())
-                exit()
-                print(tweet.created_date)
+                print(tweet)
+                print(tweet.created_at)
                 print(tweet.retweet_count * 2 + tweet.favorite_count)
                 print("")
             # end if
