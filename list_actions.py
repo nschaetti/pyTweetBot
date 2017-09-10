fb93ba630748d078f76b443cf64bf0c88de5c03c
Nils Schaetti <n.schaetti@gmail.com>
2017-09-10 23:40:44 +0200

    List action and tell if the reservoir is full

--- list_actions.py
+++ list_actions.py
@@ -23,22 +23,44 @@
 #
 
 # Import
+import sys
 
 
 # List actions
-def list_actions(action_scheduler):
+def list_actions(action_scheduler, action_type=""):
     """
     List actions
-    :return:
+    :param action_scheduler: Action Scheduler object
+    :param action_type: Filter action type
     """
     # Get actions
-    actions = action_scheduler.list_actions()
-    print(actions)
+    actions = action_scheduler.list_actions(action_type=action_type)
+
+    # Counters
+    total_action = 0
+    n_actions = {'Tweet': 0, 'Retweet': 0, 'Like': 0, 'Follow': 0, 'Unfollow': 0}
 
     # For each action
     for action in actions:
-        if action.action_type == 'tweet':
-            print(u"[{}] will tweet \"{}\"".format(action.action_tweet_text))
+        if action.action_type == 'Tweet':
+            sys.stdout.write(u"[{}] will tweet \"{}\"\n".format(action.action_id, action.action_tweet_text))
+        elif action.action_type == 'Retweet':
+            sys.stdout.write(u"[{}] will retweet {}\n".format(action.action_id, action.action_tweet_id))
+        elif action.action_type == 'Like':
+            sys.stdout.write(u"[{}] will like {}\n".format(action.action_id, action.action_tweet_id))
+        elif action.action_type == 'Follow':
+            sys.stdout.write(u"[{}] will follow \"{}\"\n".format(action.action_id, action.action_tweet_text))
+        elif action.action_type == 'Unfollow':
+            sys.stderr.write(u"[{}] will unfollow {}\n".format(action.action_id, action.action_tweet_text))
         # end if
+        n_actions[action.action_type] += 1
+        total_action += 1
+    # end for
+
+    # Type action
+    for t in n_actions.keys():
+        sys.stdout.write(u"{} action of type {} in the reservoir ({})\n".format(n_actions[t], t,
+                                                                                u"full" if action_scheduler.full(
+                                                                                    t) else u"not full"))
     # end for
 # end list_actions
