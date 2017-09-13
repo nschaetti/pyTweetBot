532112bb5b36f5d5e6398ecf410e7d25ca6a0e73
Nils Schaetti <n.schaetti@gmail.com>
2017-09-13 16:01:05 +0200

    TF-IDF model

--- learning/TFIDFModel.py
+++ learning/TFIDFModel.py
@@ -41,14 +41,17 @@ class TFIDFModel(Model):
     """
 
     # Constructor
-    def __init__(self, name, classes, last_update):
+    def __init__(self, features, name, classes):
+        """
+        Constructor
+        :param name:
+        :param classes:
+        :param last_update:
+        """
         # Superclass
-        super(TFIDFModel, self).__init__()
+        super(TFIDFModel, self).__init__(features=features, name=name, classes=classes)
 
         # Properties
-        self._name = name
-        self._classes = classes
-        self._n_classes = len(classes)
         self._n_tokens = 0.0
         self._n_total_tokens = 0.0
         self._classes_counts = dict()
@@ -57,8 +60,6 @@ class TFIDFModel(Model):
         self._classes_vectors = dict()
         self._classes_frequency = dict()
         self._token_position = dict()
-        self._last_update = last_update
-        self._finalized = False
 
         # Class counters init
         for c in classes:
@@ -75,46 +76,39 @@ class TFIDFModel(Model):
     ####################################################
 
     # Train the model
-    def train(self, text, c, lang='en'):
+    def train(self, x, y):
         """
         Train the model
-        :param text: Training text
-        :param c: Text's class
-        :param lang: Text's language
+        :param x: Training text
+        :param y: Text's class
         """
-        # Tokens
-        tokens = spacy.load(lang)(text)
-
         # For each token
-        for token in tokens:
-            token_text = token.text.lower().replace(u" ", u"").replace(u"\t", u"")
-            if len(token_text) > 1 and len(token_text) < 25 and self._filter_token(token_text):
-                # Classes counts
-                try:
-                    self._classes_counts[c][token_text] += 1.0
-                except KeyError:
-                    self._classes_counts[c][token_text] = 1.0
-                # end try
+        for token in x:
+            # Classes counts
+            try:
+                self._classes_counts[y][token] += 1.0
+            except KeyError:
+                self._classes_counts[y][token] = 1.0
+            # end try
 
-                # Collection counts
-                try:
-                    self._collection_counts[token_text] += 1.0
-                except KeyError:
-                    self._collection_counts[token_text] = 1.0
-                    self._n_tokens += 1
-                # end try
+            # Collection counts
+            try:
+                self._collection_counts[token] += 1.0
+            except KeyError:
+                self._collection_counts[token] = 1.0
+                self._n_tokens += 1
+            # end try
 
-                # Classes token count
-                try:
-                    self._classes_token_count[c] += 1.0
-                except KeyError:
-                    self._classes_token_count[c] = 1.0
-                # end try
+            # Classes token count
+            try:
+                self._classes_token_count[y] += 1.0
+            except KeyError:
+                self._classes_token_count[y] = 1.0
+            # end try
 
-                # Total tokens
-                self._n_total_tokens += 1.0
-            # end if
-        # end token
+            # Total tokens
+            self._n_total_tokens += 1.0
+        # end for
     # end train
 
     # Save the model
@@ -139,10 +133,18 @@ class TFIDFModel(Model):
         To string
         :return:
         """
-        return "TFIDFModel(name={}, n_classes={}, last_training={}, " \
-               "n_tokens={}, mem_size={}o)".format(self._name, self._n_classes,
-                                                   self._last_update, self._n_tokens,
-                                                   getsizeof(self))
+        return "TFIDFModel(name={}, n_classes={}, " \
+               "n_tokens={}, mem_size={}o)".format(self._name, self._n_classes, self._n_tokens, getsizeof(self))
+    # end __str__
+
+    # To unicode
+    def __unicode__(self):
+        """
+        To unicode
+        :return:
+        """
+        return u"TFIDFModel(name={}, n_classes={}, " \
+               u"n_tokens={}, mem_size={}o)".format(self._name, self._n_classes, self._n_tokens, getsizeof(self))
     # end __str__
 
     ###############################################
@@ -150,10 +152,10 @@ class TFIDFModel(Model):
     ###############################################
 
     # Prediction
-    def _predict(self, text, lang='en'):
+    def _predict(self, x):
         """
         Prediction
-        :param text: Text to classify
+        :param x: Text to classify
         :return: Resulting class number
         """
         # Finalize
@@ -162,26 +164,21 @@ class TFIDFModel(Model):
             self._finalized = True
         # end if
 
-        # Tokens
-        tokens = spacy.load(lang)(text)
-
+        # Compute document vector
         d_vector = np.zeros(len(self._collection_counts.keys()), dtype='float64')
-        for token in tokens:
-            token_text = token.text.lower().replace(u" ", u"").replace(u"\t", u"")
-            if len(token_text) > 1 and len(token_text) < 25 and self._filter_token(token_text):
-                try:
-                    index = self._token_position[token_text]
-                    d_vector[index] += 1.0
-                except KeyError:
-                    pass
-                # end try
-            # end if
+        for token in x:
+            try:
+                index = self._token_position[token]
+                d_vector[index] += 1.0
+            except KeyError:
+                pass
+            # end try
         # end for
 
         # Normalize vector
-        d_vector /= float(len(tokens))
+        d_vector /= float(len(x))
 
-        # For each classes
+        # Compute classes vectors
         similarity = dict()
         for index, c in enumerate(self._classes_counts.keys()):
             similarity[c] = TFIDFModel.cosinus_similarity(self._classes_vectors[c], d_vector)
@@ -251,54 +248,9 @@ class TFIDFModel(Model):
         # end for
     # end finalize
 
-    # Filter tokens
-    def _filter_token(self, token):
-        for sym in self._token_filter:
-            if sym in token:
-                return False
-            # end if
-        # end for
-        return True
-    # end _filter_token
-
     ####################################################
     # Static
     ####################################################
-
-    # Load the model
-    @staticmethod
-    def load(filename):
-        """
-        Load the model from DB or file
-        :param opt: Loading option
-        :return: The model class
-        """
-        with open(filename, 'r') as f:
-            return pickle.load(f)
-        # end with
-    # end load
-
-    # Create a new model
-    @staticmethod
-    def create(opt, n_classes=None):
-        """
-        Create a new model in DB or file
-        :param opt: Model options
-        :param n_classes: Classes count if classification model.
-        :return: The newly created model
-        """
-        pass
-    # end create
-
-    @staticmethod
-    def exists(name):
-        """
-        Does a model exists?
-        :param name: Model's name
-        :return: True or False
-        """
-        return DbModel.exists(name)
-    # end exists
 
     # Cosinus similarity
     @staticmethod
