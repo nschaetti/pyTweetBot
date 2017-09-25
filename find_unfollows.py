5a298877892d3636cfee3c4cb0b0790d84faf3e4
nschaetti <n.schaetti@gmail.com>
2017-09-26 00:06:07 +0200

    Find unfollow

--- find_unfollows.py
+++ find_unfollows.py
@@ -26,6 +26,8 @@
 import os
 import logging
 import time
+import sys
+import nsNLP
 from friends.FriendsManager import FriendsManager
 from learning.Model import Model
 from learning.CensorModel import CensorModel
@@ -45,7 +47,7 @@ from executor.ActionScheduler import ActionAlreadyExists, ActionReservoirFullErr
 
 
 # Find user to unfollow to and add it to the DB
-def find_unfollows(config, model, action_scheduler):
+def find_unfollows(config, friends_manager, model, action_scheduler, features):
     """
     Find tweet to like and add it to the DB
     :param config: Bot's configuration object
@@ -53,57 +55,82 @@ def find_unfollows(config, model, action_scheduler):
     :param action_scheduler: Action scheduler object
     :return: Number of retweets added.
     """
-    # Friends manager
-    friends_manager = FriendsManager()
-
-    # Load model or create
-    if os.path.exists(model):
-        model = Model.load(model)
+    # Load model
+    if model is not None and os.path.exists(model):
+        model = nsNLP.classifiers.TextClassifier.load(model)
         censor = CensorModel(config)
     else:
-        logging.fatal(u"Model file {} does not exists".format(args.model))
+        sys.stderr.write(u"Can't open model file {}\n".format(model))
         exit()
     # end if
 
+    # Tokenizer
+    tokenizer = nsNLP.tokenization.NLTKTokenizer(lang='english')
+
+    # Parse features
+    feature_list = features.split('+')
+
+    # Join features
+    bow = nsNLP.features.BagOfGrams()
+
+    # For each features
+    for bag in feature_list:
+        # Select features
+        if bag == 'words':
+            b = nsNLP.features.BagOfWords()
+        elif bag == 'bigrams':
+            b = nsNLP.features.BagOf2Grams()
+        elif bag == 'trigrams':
+            b = nsNLP.features.BagOf3Grams()
+        else:
+            sys.stderr.write(u"Unknown features type {}\n".format(bag))
+            exit()
+        # end if
+        bow.add(b)
+    # end for
+
     # First unfollow obsolete friends
+    logging.getLogger(u"pyTweetBot").info(u"Searching obsolete friends to unfollow")
     for friend in friends_manager.get_obsolete_friends():
         try:
-            logging.info(
+            logging.getLogger(u"pyTweetBot").info(
                 u"Adding obsolete Friend \"{}\" to unfollow to the scheduler".format(friend.friend_screen_name))
             action_scheduler.add_unfollow(friend.friend_screen_name)
         except ActionReservoirFullError:
-            logging.error(u"Reservoir full for Unfollow action, waiting for one hour")
+            logging.getLogger(u"pyTweetBot").error(u"Reservoir full for Unfollow action, waiting for one hour")
             time.sleep(3600)
             pass
         except ActionAlreadyExists:
-            logging.error(
+            logging.getLogger(u"pyTweetBot").error(
                 u"Unfollow action for \"{}\" already exists in the database".format(friend.friend_screen_name))
             pass
         # end try
     # end for
 
     # Find friends to unfollow
-    """for page in twitter_connector.get_following_cursor().pages():
-        # For each followers
-        for friend in page:
+    logging.getLogger(u"pyTweetBot").info(u"Searching useless friends to unfollow")
+    for friend in friends_manager.get_following():
+        if not FriendsManager.is_follower(friend.friend_screen_name):
             # Predict class
-            prediction, = model(friend)
+            prediction, = model(friend.friend_description)
+            censor_prediction, _ = censor(friend.friend_description)
 
             # Predicted as unfollow
-            if prediction == "unfollow":
+            if prediction == 'neg' or censor_prediction == 'neg':
                 try:
-                    logging.info(u"Adding Friend \"{}\" to unfollow to the scheduler".format(friend.friend_screen_name))
+                    logging.getLogger(u"pyTweetBot").info(
+                        u"Adding Friend \"{}\" to unfollow to the scheduler".format(friend.friend_screen_name))
                     action_scheduler.add_unfollow(friend.friend_screen_name)
                 except ActionReservoirFullError:
-                    logging.error(u"Reservoir full for Unfollow action, waiting for one hour")
-                    time.sleep(3600)
+                    logging.getLogger(u"pyTweetBot").error(u"Reservoir full for unfollow action, exiting...")
+                    exit()
                     pass
                 except ActionAlreadyExists:
-                    logging.error(
+                    logging.getLogger(u"pyTweetBot").error(
                         u"Unfollow action for \"{}\" already exists in the database".format(friend.friend_screen_name))
                     pass
                 # end try
             # end if
-        # end for
-    # end for"""
+        # end if
+    # end for
 # end find_follows
