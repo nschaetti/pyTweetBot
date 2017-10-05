fe5d2a76e55d30d104b359a13a38024426ce891a
nschaetti <n.schaetti@gmail.com>
2017-10-05 10:30:34 +0200

    Database

--- db/__init__.py
+++ db/__init__.py
@@ -3,8 +3,8 @@
 #
 
 # Import
-from .DBConnector import DBConnector
-from .obj.Action import Action
+from DBConnector import DBConnector
+"""from .obj.Action import Action
 from .obj.Base import Base
 from .obj.Follower import Follower
 from .obj.Following import Following
@@ -13,4 +13,4 @@ from .obj.Model import Model
 from .obj.ModelTokens import ModelToken
 from .obj.Statistic import Statistic
 from .obj.ImpactStatistics import ImpactStatistic
-from .obj.Tweeted import Tweeted
+from .obj.Tweeted import Tweeted"""
