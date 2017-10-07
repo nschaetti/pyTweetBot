a069ebd9517be0f3ed7e558560cf5c07e1fe74cb
nschaetti <n.schaetti@gmail.com>
2017-10-07 21:56:35 +0200

    Find retweets

--- find_retweets.py
+++ find_retweets.py
@@ -107,26 +107,33 @@ def find_retweets(config, model, action_scheduler, features, text_size=80, retwe
         # For each tweet
         for retweet, polarity, subjectivity in retweet_finder:
             # Minimum size
-            if len(retweet.text) >= text_size:
+            if len(retweet.text) >= text_size and retweet.text[:3] != u"RT ":
                 # Predict class
                 prediction, _ = model(bow(tokenizer(retweet.text)))
                 censor_prediction, _ = censor(retweet.text)
 
                 # Predicted as tweet
-                if prediction == "pos" and censor_prediction == "pos" and not Tweeted.exists(retweet.id):
+                if prediction == "pos" and censor_prediction == "pos" and not Tweeted.exists(retweet):
                     # Decide which action (retweet, like)
                     random_action = actions[random.randint(0, 2)-1]
 
                     # Try to add
                     try:
-                        logging.getLogger(u"pyTweetBot").info(u"Adding {} \"{}\" to the scheduler".format(random_action,
-                            retweet.text.encode('ascii', errors='ignore')))
+                        logging.getLogger(u"pyTweetBot").\
+                        info(
+                            u"Adding {} ({}, \"{}\") to the scheduler".
+                            format(
+                                random_action,
+                                retweet.id,
+                                retweet.text.encode('ascii', errors='ignore')
+                            )
+                        )
 
                         # Add action
                         if random_action == u"retweet":
-                            action_scheduler.add_retweet(retweet.id)
+                            action_scheduler.add_retweet(retweet.id, retweet.text)
                         else:
-                            action_scheduler.add_like(retweet.id)
+                            action_scheduler.add_like(retweet.id, retweet.text)
                         # end if
                     except ActionReservoirFullError:
                         logging.getLogger(u"pyTweetBot").error(u"Reservoir full for Retweet action, exiting...")
