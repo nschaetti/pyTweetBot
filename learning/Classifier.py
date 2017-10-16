4eeae4432905bf7f45b51b7021a6d836d6ef210c
nschaetti <n.schaetti@gmail.com>
2017-10-16 23:02:41 +0200

    Classifier base class

---
+++ learning/Classifier.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : Classifier.py
+# Description : Classifier base class
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Imports
+import nsNLP
+import sys
+
+
+# Classifier base class
+class Classifier(object):
+    """
+    Classifier
+    """
+
+    # Constructor
+    def __init__(self, features, language='english'):
+        """
+        Constructor
+        :param config: Settings
+        """
+        # Tokenizer
+        self._tokenizer = nsNLP.tokenization.NLTKTokenizer(lang=language)
+
+        # Parse features
+        feature_list = features.split('+')
+
+        # Join features
+        self._bow = nsNLP.features.BagOfGrams()
+
+        # For each features
+        for bag in feature_list:
+            # Select features
+            if bag == 'words':
+                b = nsNLP.features.BagOfWords()
+            elif bag == 'bigrams':
+                b = nsNLP.features.BagOf2Grams()
+            elif bag == 'trigrams':
+                b = nsNLP.features.BagOf3Grams()
+            else:
+                sys.stderr.write(u"Unknown features type {}".format(features))
+                exit()
+            # end if
+            self._bow.add(b)
+        # end for
+    # end __init__
+
+    #################################################
+    # Public
+    #################################################
+
+    #################################################
+    # Override
+    #################################################
+
+    # Predict
+    def __call__(self, x):
+        """
+        Predict
+        :param x: Text to classify
+        :return:
+        """
+        pass
+    # end __call__
+
+    #################################################
+    # Private
+    #################################################
+
+    # Predict
+    def _predict(self, x):
+        """
+        Predict
+        :param x:
+        :return:
+        """
+        pass
+    # end _predict
+
+    #################################################
+    # Static
+    #################################################
+
+# end Model
+
