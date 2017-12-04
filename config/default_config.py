942964fe1ece4cc38a22706f076693699daee8f4
Nils Schaetti <n.schaetti@gmail.com>
2017-12-04 18:04:53 +0100

    Default configuration
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

---
+++ config/default_config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Default configuration
+default_config = \
+{
+	"scheduler" :
+	{
+		"sleep": [6, 13]
+	},
+	"hashtags":
+	[
+	],
+	"friends" :
+	{
+		"max_new_followers" : 40,
+		"max_new_unfollow" : 40,
+		"follow_unfollow_ratio_limit" : 1.2,
+		"interval" : [30, 45]
+	},
+	"forbidden_words" :
+	[
+	],
+	"direct_message" : "",
+	"news_settings" : {
+		"max_tweets" : 1200,
+		"exclude" : [],
+		"interval" : [2.0, 4.0]
+	},
+	"news" :
+	[
+	],
+	"rss" :
+	[
+	],
+	"retweet" :
+	{
+		"max_retweets" : 600,
+		"max_likes" : 600,
+		"keywords" : [],
+		"nbpages" : 40,
+		"retweet_prob" : 0.5,
+		"limit_prob" : 1.0
+		"interval" : [2.0, 4.0]
+	}
+}
