b37d32040bbb4b7ee99c46f381e0c107e79c6592
nschaetti <n.schaetti@gmail.com>
2017-12-05 00:15:27 +0100

    Complete list of required fields

---
+++ config/required_fields.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+required_fields = \
+{
+    "database":
+    {
+        "host": {},
+        "username": {},
+        "password": {},
+        "database": {}
+    },
+    "twitter":
+    {
+        "auth_token1": {},
+        "auth_token2": {},
+        "access_token1": {},
+        "access_token2": {},
+        "user": {}
+    }
+}
