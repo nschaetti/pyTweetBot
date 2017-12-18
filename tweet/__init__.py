7488670c079efc641d30563f7d48d70137ae3e8e
Nils Schaetti <n.schaetti@gmail.com>
2017-12-18 10:24:41 +0100

    Tweet package cleaned and commented
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

--- tweet/__init__.py
+++ tweet/__init__.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : tweet.__init__.py
+# Description : pyTweetBot Twitter connector.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 from .Hunter import Hunter
 from .RSSHunter import RSSHunter
