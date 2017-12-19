df8f4088a534b88fd9e8532b40587c336b215b53
Nils Schaetti <n.schaetti@gmail.com>
2017-12-19 11:05:03 +0100

    Cleaned page parser
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

--- tools/PageParser.py
+++ tools/PageParser.py
@@ -1,15 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# File : mail.PageParser.py
+# Description : Page parser object.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 # Import
-import socket
-import sys
 import urllib2
-import httplib
 import brotli
 import gzip
-import logging
 from urlparse import urlparse
 from bs4 import BeautifulSoup
 from StringIO import StringIO
