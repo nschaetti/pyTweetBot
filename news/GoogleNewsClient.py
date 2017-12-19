d6eb9b1ef90b5efa14d962866859f47855de6ed4
Nils Schaetti <n.schaetti@gmail.com>
2017-12-19 11:04:32 +0100

    Cleaned news package
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

--- news/GoogleNewsClient.py
+++ news/GoogleNewsClient.py
@@ -1,5 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# File : news.GoogleNewsClient.py
+# Description : Google News client.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 # Import
@@ -7,7 +27,6 @@ import urllib2
 from urlparse import urlparse
 from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
-import numpy as np
 import time
 import random
 from .NewsParser import NewsParser
--- news/NewsParser.py
+++ news/NewsParser.py
@@ -1,5 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# File : news.NewsParser.py
+# Description : Google News client.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 # Import
@@ -16,13 +36,12 @@ class NewsParser(HTMLParser):
     It returns an array containing the URLs.
     """
 
-    # handle_starttag
+    # Handle startag
     def handle_starttag(self, tag, attrs):
         """
-
-        :param tag:
-        :param attrs:
-        :return:
+        Handle startag
+        :param tag: Tag to handle
+        :param attrs: Tag's attributes
         """
         # Init
         try:
@@ -62,6 +81,10 @@ class NewsParser(HTMLParser):
 
     # Get news
     def get_news(self):
+        """
+        Get the news
+        :return:
+        """
         return self.news
     # end get_news
 
