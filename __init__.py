5511604932be524327fbdffbb67ceef0d56bcccb
Nils Schaetti <n.schaetti@gmail.com>
2017-12-20 20:09:14 +0100

    Root init
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

---
+++ __init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# File : pyTweetBot.py
+# Description : pyTweetBot main execution file.
+# Auteur : Nils Schaetti <n.schaetti@gmail.com>
+# Date : 01.05.2017 17:59:05
+# Lieu : Nyon, Suisse
+#
+# This file is part of the pyTweetBot.
+# The pyTweetBot is a set of free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pyTweetBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with pyTweetBar.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Import
+import config
+import db
+import directmessages
+import executor
+import friends
+import learning
+import mail
+import news
+import patterns
+import retweet
+import stats
+import templates
+import tools
+import tweet
+import twitter
