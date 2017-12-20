00b208559e93e41b091590ce12962fdea342d29a
Nils Schaetti <n.schaetti@gmail.com>
2017-12-20 19:51:47 +0100

    Bug get instance
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

--- friends/FriendsManager.py
+++ friends/FriendsManager.py
@@ -29,6 +29,7 @@ import db.obj
 from patterns.singleton import singleton
 from twitter.TweetBotConnect import TweetBotConnector
 #from pyTweetBot.twitter.TweetBotConnect import TweetBotConnector
+import pyTweetBot
 import twitter
 from sqlalchemy import update, delete
 from sqlalchemy.orm import load_only
@@ -76,7 +77,7 @@ class FriendsManager(object):
         self._session = db.DBConnector().get_session()
 
         # Twitter connection
-        self._twitter_con = TweetBotConnector()
+        self._twitter_con = pyTweetBot.twitter.TweetBotConnector()
 
         # Logger
         self._logger = logging.getLogger(name=u"pyTweetBot")
