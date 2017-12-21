2bbf0a1288ec8ea0b84ce0bc9ac7eda917ec1068
Nils Schaetti <n.schaetti@gmail.com>
2017-12-22 00:44:21 +0100

    Setup
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

--- setup.py
+++ setup.py
@@ -2,5 +2,22 @@ from setuptools import setup
 
 setup(name='pyTweetBot',
       version='0.1',
-      description='A Twitter bot to collect articles and news from the Web and tweet/retweet it on your feed'
-      )
+      description='A Twitter bot to collect articles and news from the Web and tweet/retweet it on your feed',
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary'
+      ],
+      keywords='twitter bot nlp news tweet',
+      url='https://github.com/nschaetti/pyTweetBot',
+      author='Nils Schaetti',
+      author_email='n.schaetti@gmail.com',
+      license='GPL',
+      include_package_data=True,
+      packages=['config', 'db', 'directmessages', 'executor', 'friends', 'learning', 'mail', 'news', 'patterns',
+                'retweet', 'stats', 'templates', 'tools', 'tweet', 'twitter'],
+      install_requires=['nltk', 'argparse', 'logging', 'tweepy', 'sklearn', 'pygithub', 'brotli', 'httplib2',
+                        'urlparse2', 'HTMLParser', 'bs4', 'simplejson', 'dnspython'],
+      test_suite='nose.collector',
+      tests_require=['nose']
+)
