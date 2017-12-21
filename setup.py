dcce5438c4977cc50e36b7cb16474176a4e0085e
Nils Schaetti <n.schaetti@gmail.com>
2017-12-21 23:58:35 +0100

    Setup
    Signed-off-by: Nils Schaetti <n.schaetti@gmail.com>

--- setup.py
+++ setup.py
@@ -2,10 +2,5 @@ from setuptools import setup
 
 setup(name='pyTweetBot',
       version='0.1',
-      description='A Twitter bot to collect articles and news from the Web and tweet/retweet it on your feed',
-      url='https://github.com/nschaetti/pyTweetBot',
-      author='Nils Schaetti',
-      author_email='n.schaetti@gmail.com',
-      license='GPL',
-      packages=['config', 'db', 'directmessages', 'executor', 'friends', 'learning', 'mail', 'news', 'patterns',
-                'retweet', 'stats', 'templates', 'tools', 'tweet', 'twitter'])
+      description='A Twitter bot to collect articles and news from the Web and tweet/retweet it on your feed'
+      )
