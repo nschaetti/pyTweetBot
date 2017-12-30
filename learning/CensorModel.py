4b2caf9071317262b0f1c4ce3a9636b37fd57e21
nschaetti <n.schaetti@gmail.com>
2017-12-30 01:11:17 +0100

    Remove debug print

--- learning/CensorModel.py
+++ learning/CensorModel.py
@@ -59,7 +59,6 @@ class CensorModel(object):
         # For each forbidden word
         for word in self._forbidden_words:
             if word.lower() in x.lower():
-                print(u"{} in \"{}\"".format(word.lower(), x.lower()))
                 return 'neg', {'neg': 1.0, 'pos': 0.0}
             # end if
         # end for
