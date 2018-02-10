d5abadcc7e45e31a77a12bc600a99b3589c2a96b
nschaetti <n.schaetti@gmail.com>
2018-02-10 16:48:04 +0100

    Remove debug print

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='pyTweetBot',
                 'retweet', 'stats', 'templates', 'tools', 'tweet', 'twitter'],
       install_requires=['nltk', 'argparse', 'logging', 'tweepy', 'sklearn', 'pygithub', 'brotli', 'httplib2',
                         'urlparse2', 'HTMLParser', 'bs4', 'simplejson', 'dnspython', 'dill', 'lxml', 'sqlalchemy',
-                        'feedparser', 'textblob', 'numpy', 'scipy'],
+                        'feedparser', 'textblob', 'numpy', 'scipy', 'mysql-python'],
       test_suite='nose.collector',
       tests_require=['nose']
 )
